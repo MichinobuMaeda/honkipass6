241260bcaed31563751d1e3b2c88176281cbb2ef
michinobumaeda <michinobumaeda@gmail.com>
2025-12-02 08:53:14 +0000

    fix: deployment to subdirectory

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4031851645"
+    serviceWorkerVersion: "3204783882"
   }
 });
